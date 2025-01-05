3c85ff3d74d5586000e5662892d42db513925896
saurav-prasad <sauravprasad2050@gmail.com>
2025-01-05 16:20:16 +0530

    some updation

--- routes/canvas.js
+++ routes/canvas.js
@@ -10,33 +10,39 @@ const jwtSecret = process.env.JWT_SECRET
 const fetchUser = require('../middleware/fetchUser')
 
 // Create fetchedCanvas - POST request
-router.post('/createcanvas', fetchUser, async (req, res) => {
-    let success = false
-    try {
-        const fetchedUserId = req.userId
-        const { fetchedCanvas, name, userId } = req.body
-        // check if fetchcedUserId and userId are same
-        if (fetchedUserId !== userId) {
-            return res.status(401).json({ success, message: "Unauthorized - Cannot create" })
+router.post('/createcanvas', fetchUser,
+    [body('name', "Name should be atleast of 1 characters").isLength({ min: 1 })],
+    async (req, res) => {
+        let success = false
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ success, message: errors.array()[0].msg })
         }
-        // create fetchedCanvas
-        const newCanvas = await canvasSchema.create({ fetchedCanvas, name, userId })
-
-        success = true
-        res.send({
-            success, message: "Canvas created successfully", data: {
-                id: newCanvas.id,
-                fetchedCanvas: newCanvas.fetchedCanvas,
-                name: newCanvas.name,
-                userId: newCanvas.userId,
-                timestamp: newCanvas.timestamp
+        try {
+            const fetchedUserId = req.userId
+            const { name, userId } = req.body
+            // check if fetchcedUserId and userId are same
+            if (fetchedUserId !== userId) {
+                return res.status(401).json({ success, message: "Unauthorized - Cannot create" })
             }
-        })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ success, message: "Internal server error" })
+            // create fetchedCanvas
+            const newCanvas = await canvasSchema.create({ name, userId })
+
+            success = true
+            res.send({
+                success, message: "Canvas created successfully", data: {
+                    _id: newCanvas.id,
+                    canvas: newCanvas.canvas,
+                    name: newCanvas.name,
+                    userId: newCanvas.userId,
+                    timestamp: newCanvas.timestamp
+                }
+            })
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json({ success, message: "Internal server error" })
+        }
     }
-}
 )
 
 // Delete fetchedCanvas by id - post request
@@ -109,11 +115,15 @@ router.get('/getallcanvases', fetchUser, async (req, res) => {
         const userId = req.userId
         // fetch all canvases
         const fetchedCanvases = await canvasSchema.find({ userId })
-        .select('name _id userId timestamp')
+            .select('name _id userId timestamp')
+            .sort({ timestamp: 'desc' })
+
         //if canvases not found
         if (fetchedCanvases.length <= 0) {
-            return res.status(404).json({ success, message: "Canvases not found" })
+            return res.json({ success, message: "Canvases not found" })
         }
+
+        console.log(fetchedCanvases)
         success = true
         res.send({
             success, message: "Canvases fetched successfully", data: fetchedCanvases
@@ -126,42 +136,49 @@ router.get('/getallcanvases', fetchUser, async (req, res) => {
 )
 
 // Update fetchedCanvas name by id - POST request
-router.post('/updatecanvasname/:canvasid', fetchUser, async (req, res) => {
-    let success = false
-    try {
-        const canvasId = req.params.canvasid
-        const userId = req.userId
-        const { name } = req.body
-        // check if fetchedCanvas id exists
-        if (!canvasId) {
-            return res.status(404).json({ success, message: "Canvas id required" })
-        }
-        // check if fetchedCanvas exists
-        const fetchedCanvas = await canvasSchema.findById(canvasId)
-        if (!fetchedCanvas) {
-            return res.status(404).json({ success, message: "Canvas not found" })
-        }
-        // check if userId and fetchedCanvas.userId are same
-        if (userId !== fetchedCanvas.userId.toString()) {
-            return res.status(401).json({ success, message: "Unauthorized - Cannot update" })
+router.post('/updatecanvasname/:canvasid', fetchUser,
+    [body('name', "Name should be atleast of 1 characters").isLength({ min: 1 })],
+    async (req, res) => {
+        let success = false
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ success, message: errors.array()[0].msg })
         }
 
-        // check if previous name and new name are same
-        if (name === fetchedCanvas.name) {
-            return res.status(401).json({ success, message: "Same name - cannot update" })
+        try {
+            const canvasId = req.params.canvasid
+            const userId = req.userId
+            const { name } = req.body
+            // check if fetchedCanvas id exists
+            if (!canvasId) {
+                return res.status(404).json({ success, message: "Canvas id required" })
+            }
+            // check if fetchedCanvas exists
+            const fetchedCanvas = await canvasSchema.findById(canvasId)
+            if (!fetchedCanvas) {
+                return res.status(404).json({ success, message: "Canvas not found" })
+            }
+            // check if userId and fetchedCanvas.userId are same
+            if (userId !== fetchedCanvas.userId.toString()) {
+                return res.status(401).json({ success, message: "Unauthorized - Cannot update" })
+            }
+
+            // check if previous name and new name are same
+            if (name === fetchedCanvas.name) {
+                return res.status(401).json({ success, message: "Same name - cannot update" })
+            }
+
+            // update fetchedCanvas name
+            fetchedCanvas.name = name
+            await fetchedCanvas.save()
+
+            success = true
+            res.send({ success, message: "Canvas name updated successfully" })
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json({ success, message: "Internal server error" })
         }
-
-        // update fetchedCanvas name
-        fetchedCanvas.name = name
-        await fetchedCanvas.save()
-
-        success = true
-        res.send({ success, message: "Canvas name updated successfully" })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ success, message: "Internal server error" })
     }
-}
 )
 
 // Update canvas by id - POST request
